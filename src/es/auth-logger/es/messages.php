211fd5a4d2a2286c5766b022e1c14b4ee8f8eab8
TechTailor <moinuddinsk210@gmail.com>
2020-12-13 19:05:56 +0530

    Updated Auth Logger (Spanish)

--- src/es/auth-logger/es/messages.php
+++ src/es/auth-logger/es/messages.php
@@ -4,18 +4,23 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Log Language Lines
+    | Auth Logger Language Lines
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used during authentication log for
+    | The following language lines are used by the auth logger package for
     | various messages that we need to display to the user. You are free to
     | modify these language lines according to your application's requirements.
-    |
-    | Update: This file has been translated.
     |
     */
 
     'subject' => '[Alerta] Iniciar sesión desde un nuevo dispositivo',
+    'hello' => 'Hola',
+    'regards' => 'Saludos',
     'content' => 'Su cuenta de :app inició sesión desde un nuevo dispositivo.',
-
+    'account' => 'Email',
+    'time' => 'Hora',
+    'ip_address' => 'Dirección IP',
+    'browser' => 'Navegador',
+    'platform' => 'Platform',
+    'ignore' => 'If this was you, you can ignore this alert. If you suspect any suspicious activity on your account, please change your password.'
 ];
