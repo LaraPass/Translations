403f48df4510788e8c10a9ebe39f3e4cdc0740d9
TechTailor <moinuddinsk210@gmail.com>
2020-12-13 19:02:59 +0530

    Updated Auth Logger Language Lines

--- src/en/vendor/auth-logger/en/messages.php
+++ src/en/vendor/auth-logger/en/messages.php
@@ -4,16 +4,23 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Log Language Lines
+    | Auth Logger Language Lines
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used during authentication log for
+    | The following language lines are used by the auth logger package for
     | various messages that we need to display to the user. You are free to
     | modify these language lines according to your application's requirements.
     |
     */
 
     'subject' => '[Alert] Login from a new device detected',
+    'hello' => 'Hello',
+    'regards' => 'Regards',
     'content' => 'Your :app account was logged in from a new device.',
-
+    'account' => 'Account',
+    'time' => 'Time',
+    'ip_address' => 'IP Address',
+    'browser' => 'Browser',
+    'platform' => 'Platform',
+    'ignore' => 'If this was you, you can ignore this alert. If you suspect any suspicious activity on your account, please change your password.'
 ];
