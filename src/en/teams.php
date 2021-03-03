dddf4fcb1ddb459edb97e86ab1ff6ce2e5c969f5
TechTailor <moinuddinsk210@gmail.com>
2021-03-03 18:03:30 +0530

    Teams Additions

---
+++ src/en/teams.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language Lines for the Teams Components.
+    |--------------------------------------------------------------------------
+    */
+
+    'team' => 'Team',
+    'teams' => 'Teams',
+    'id' => 'ID',
+    'my_teams' => 'My Teams',
+    'members' => 'Members',
+    'admin' => 'Admin',
+    'member' => 'Member',
+    'intern' => 'Intern',
+    'team_menu' => 'Team Menu',
+    'invite_title' => 'Invite a New User to join <b>:team</b>',
+    'invite_email_placeholder' => 'Enter the email address of the user you want to invite to this team.',
+    'message' => 'Welcome Message',
+    'invite_message_placeholder' => 'Write a welcome message identifying yourself to the user.',
+    'invite_user' => 'Invite User',
+    'change_role' => 'Change Role',
+    'remove_member' => 'Remove Member',
+    'leave_modal_head' => 'Do you really want to leave <b>:team</b>.',
+    'leave_modal_body' => 'Once confirmed, you will be removed from this team. Once removed, you will lose access to any team data (vaults, sites, notes, etc). You can be invited back into the team by the Team Owner/Admin.',
+    'confirm_removal' => 'Yes, Confirm Removal',
+    'roles' => 'Roles',
+    'role' => 'Role',
+    'permissions' => 'Permissions',
+    'permission' => 'Permission',
+    'leave_team' => 'Leave Team',
+    'change_role_to' => 'Change User Role To',
+    'no_permission' => 'Sorry, you do not have the required permission to perform this action. Contact Team Admin to upgrade your permissions.',
+    'role_updated_alert' => 'User role has been updated to :role Role.',
+    'limit_reached' => 'Sorry! Teams Limit Reached. You cannot create any more teams.',
+    'member_limit_reached' => 'Sorry! Team Members Limit Reached. You cannot add any more members to this team.',
+    'cannot_create_new_teams' => 'Sorry! You do not have the permission to create new teams. Contact Site Administrator if its a mistake.',
+    'not_part_of_any_team' => 'You are not part of any team. Create a new one to get started.',
+    'my_invitations' => 'My Team Invitations',
+    'accept_invite' => 'Accept Invite',
+    'reject_invite' => 'Reject Invite',
+    'alerts' => [
+        'self_invite_error' => 'Sorry, you cannot invite yourself to the team',
+        'email_address_required' => 'Email address of the user is required',
+        'email_doesnt_exists' => 'Sorry, this email doesnt not belong to any user.',
+        'user_already_part_of_team' => 'This user is already a member of this team',
+        'message_required' => 'Welcome Message is required.',
+        'max_teams_joined' => 'You have reached the maximum no. of teams allowed. Leave any existing teams to join a new one.',
+        'teams_disabled' => 'The Teams Module is currently disabled. No User can create or access any Teams data. To enable the Teams Module, click <a href=":url">here</a>.',
+        'max_members_required' => 'Max. members limit is required',
+    ],
+    'table' => [
+        'id' => 'ID',
+        'name' => 'Team Name',
+        'description' => 'Description',
+        'message' => 'Message',
+        'invited_by' => 'Invited By',
+        'invited_on' => 'Invited On',
+        'joining_as' => 'Joining As',
+        'team_members' => 'Team Members',
+    ],
+];
